99b30aba56a691d3c0803a17be48737398a2b562
andrejvb <andre.jvb@gmail.com>
2024-10-24 17:45:21 -0300

    configura metronic e cria tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+import KTComponents from '../metronic/core/index';
+import KTLayout from '../metronic/app/layouts/demo1';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Note que 'styleUrls' está no plural
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'azzo-frontend';
+
+  ngAfterViewInit(): void {
+    KTComponents.init();
+    KTLayout.init();
+  }
 }
